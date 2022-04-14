657de9baf2b4c485fc22b55f9e7845cafe61fb18
John Lago <750845+Lagoja@users.noreply.github.com>
2022-04-14 16:22:37 -0700

    initial commit

---
+++ api/app/main.py
@@ -0,0 +1,12 @@
+from typing import Dict
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root() -> Dict[str, str]:
+    return {
+        "message": "Hello World!"
+    }
