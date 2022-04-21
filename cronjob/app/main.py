8083582e214539cafc4e1cf395061be47d66f9fc
John Lago <750845+Lagoja@users.noreply.github.com>
2022-04-21 09:25:05 -0700

    fix typo in cronjob

--- cronjob/app/main.py
+++ cronjob/app/main.py
@@ -1,7 +1,7 @@
 from jetpack import cron
 
 
-@cron.repeat(cron.every(10).minute)
+@cron.repeat(cron.every(10).minutes)
 async def cron_job_function():
     ### Code for your cronjob goes here
     print("I run every 10 minutes")
